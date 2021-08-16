c38c8fc2997d832ba09351178ea0c647fa2ae6e8
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-16 14:45:26 -0400

    commit tree 06ec78676bba061ae4c973d272d829e717d7f944

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(dvrs) {
+    return dvrs.slice(0,2)
+}
+
+const returnLastTwoDrivers= function(drvs) {
+    const chooseDrvs = drvs.slice(drvs.length-2,drvs.length)
+    return chooseDrvs
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number) {
+    return function(fare) {
+        return fare*number
+    }
+}
+
+let fareDoubler = createFareMultiplier(2)
+let fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, action){
+    return action(drivers)
+}
